cf56ebf1b528620bc29027cde3cd10513361e8ec
huzr <huzhui.x@gmail.com>
2019-11-10 21:19:15 +0800

    放弃c++，改用python

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 #include "RSA.hpp"
 
 
+extern string private_key, p_bin, q_bin, r_bin, a_bin;
 using namespace std;
 int main(){
-    cout << private_key << endl;
+    RSA rsa(p_bin, q_bin, a_bin);
+    rsa.test();
     return 0;
 }
